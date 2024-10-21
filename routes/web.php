b9bc32674de33d817d7eb73cd0f1a383776eb72c
RespawnHood <ammarzann@gmail.com>
2024-10-21 09:23:39 +0700

    feat:auth

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/home', '/');
-
 Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('home', function () {
     return view('pages.dashboard');
 })->name('dashboard');
